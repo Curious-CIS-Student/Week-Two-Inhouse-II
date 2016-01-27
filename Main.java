de989dd21afac5da134b954414c7bc20d0fef0e9
Curious-CIS-Student <isaac.blasiman@myemail.indwes.edu>
2016-01-27 12:04:02 -0500

    Made changes

--- Main.java
+++ Main.java
@@ -1,26 +1,90 @@
+/**
+ * STUDENT:     Isaac Blasiman
+ * CLASS:       CIS222 Object Oriented Programming
+ * INSTRUCTOR:  Dr. Neumann
+ * ASSIGNMENT:  Week-Two-Inhouse-II
+ */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        /*
-        Step 1:  Create several instances of the Money class using the various constructors.
-        Test the class by printing out the values using the getter methods and the toString method.
-        Test the add method by adding together two instances of Money objects.
-        Remember that the add method returns a new object of the type Money. It does NOT modify
-        any values in the existing objects.
+        // Step 1:  Create several instances of the Money class using the various constructors.
+        System.out.println("Creating three Money objects money1, money2, and money3,");
+        System.out.println("with the monetary values $0.00, $53.30, and 4.60 respectively.");
+        System.out.println();
+        Money money1 = new Money();
+        Money money2 = new Money(53,30);
+        Money money3 = new Money(4,60);
 
-        Does the Money class correctly handle all possibilities for input?  Did you check edge cases?
-        Also, for the purpose of this exercise, the Money object should not allow negative values.
+        //Test the class by printing out the values using the getter methods and the toString method.
+        System.out.println("TESTING PRINTING VALUES");
+        System.out.println("Dollars and cents for money1 are " + money1.getDollars() +
+                           " and " + money1.getCents() + ", and " +
+                           "money1 is displayed as " + money1.toString() + ".");
 
+        System.out.println("Dollars and cents for money2 are " + money2.getDollars() +
+                " and " + money2.getCents() + ", and " +
+                "money2 is displayed as " + money2.toString() + ".");
 
-        Step 2:  Have the parameterized constructor normalize the arguments. That is, convert cents to dollars and cents if cents > 99.
-        Modify the add method to normalize the returned object as well.
-        Modify the constructor to zero out any negative inputs.
+        System.out.println("Dollars and cents for money3 are " + money3.getDollars() +
+                " and " + money3.getCents() + ", and " +
+                "money3 is displayed as " + money3.toString() + ".");
+        System.out.println();
 
-        Step 3:  Include a subtract method that subtracts the paramater from the instance to which it is applied.
-        There are several ways we can handle a resulting negative amount.  We could crash the program,
-        simply zero out the result, ignore the method if the result is zero, or throw an exception.
-        For now, if the result is negative, zero out the returning object and print an error message.  Later we will look at how to throw
-        an exception.*/
+        //Test the add method by adding together two instances of Money objects.
+        System.out.println("TESTING THE ADD() METHOD");
+        System.out.println("money2 + money3 = " + (money2.add(money3)).toString() + ".");
+        System.out.println();
+
+        //Does the Money class correctly handle all possibilities for input?  Did you check edge cases?
+        //Also, for the purpose of this exercise, the Money object should not allow negative values.
+        System.out.println("TESTING CONSTRUCTOR FOR DISALLOWING NEGATIVE VALUES");
+        System.out.println("Result of trying to create a money object called negativeDollars with a negative dollars value:");
+        Money negativeDollars = new Money(-3,0);
+        System.out.println("Dollars and cents for negativeDollars are " + negativeDollars.getDollars() +
+                " and " + negativeDollars.getCents() + ", and " +
+                "negativeDollars is displayed as " + negativeDollars.toString() + ".");
+        System.out.println();
+
+        System.out.println("Result of trying to create a money object called negativeCents with a negative cents value:");
+        Money negativeCents = new Money(6,-25);
+        System.out.println("Dollars and cents for negativeCents are " + negativeCents.getDollars() +
+                " and " + negativeCents.getCents() + ", and " +
+                "negativeCents is displayed as " + negativeCents.toString() + ".");
+        System.out.println();
+
+        //Step 2:  Have the parameterized constructor normalize the arguments. That is, convert cents to dollars and cents if cents > 99.
+        //Modify the add method to normalize the returned object as well.
+        //Modify the constructor to zero out any negative inputs.
+        System.out.println("TESTING NORMALIZING THE ARGUMENTS TO THE CONSTRUCTOR");
+        System.out.println("Creating a new money object tooManyCents with dollars value of 10 and cents value of 225.");
+        Money tooManyCents = new Money(10,225);
+        System.out.println("Dollars and cents for tooManyCents are " + tooManyCents.getDollars() +
+                " and " + tooManyCents.getCents() + ", and " +
+                "tooManyCents is displayed as " + tooManyCents.toString() + ".");
+        System.out.println();
+
+        System.out.println("TESTING NORMALIZING THE ARGUMENTS TO THE ADD() METHOD");
+        System.out.println("Creating two money objects addMoney1 and addMoney2, with monetary values of $3.60 and $5.80 respectively.");
+        Money addMoney1 = new Money(3,60);
+        Money addMoney2 = new Money(5,80);
+        System.out.println("addMoney1 + addMoney2 = " + addMoney1.add(addMoney2));
+        System.out.println();
+
+        //Step 3:  Include a subtract method that subtracts the parameter from the instance to which it is applied.
+        //There are several ways we can handle a resulting negative amount.  We could crash the program,
+        //simply zero out the result, ignore the method if the result is zero, or throw an exception.
+        //For now, if the result is negative, zero out the returning object and print an error message.  Later we will look at how to throw
+        //an exception.
+        System.out.println("TESTING SUBTRACT() METHOD");
+        System.out.println("Creating two new money objects, biggerMoney and smallerMoney, with monetary values of $6.25 and 4.50 respectively.");
+        Money biggerMoney = new Money(6,25);
+        Money smallerMoney = new Money(4,50);
+        System.out.println("biggerMoney.subtract(smallerMoney) = " + biggerMoney.subtract(smallerMoney) + ".");
+        System.out.println();
+        System.out.println("smallerMoney.subtract(biggerMoney) results in: ");
+        Money result = (smallerMoney.subtract(biggerMoney));
+        System.out.println("The dollars and cents values for the object returned by smallerMoney.subtract(biggerMoney) are " +
+                result.getDollars() + " and " + result.getCents() + ".");
+        System.out.println("The object returned a display value of " + result.toString() + ".");
     }
 }
--- Money.java
+++ Money.java
@@ -1,28 +1,78 @@
+/**
+ * STUDENT:     Isaac Blasiman
+ * CLASS:       CIS222 Object Oriented Programming
+ * INSTRUCTOR:  Dr. Neumann
+ * ASSIGNMENT:  Week-Two-Inhouse-II
+ */
 public class Money {
     private long dollars;
     private long cents;
 
     // Constructors
-// Made a change
+
     /**
      * Empty constructor that sets dollars and cents to zero.
      */
+    public Money() {
+        dollars = 0;
+        cents = 0;
+    }
 
     /**
      * Constructor with parameter for dollars, cents
      */
+    public Money(long dollars, long cents) {
+        // Check that the amounts entered are reasonable.
+        if (dollars >= 0 && cents >= 0){
+            if (cents < 100) {
+                this.dollars = dollars;
+                this.cents = cents;
+            }
+            else { // Need to "roll over" extra cents over 100 into another dollar.
+                this.dollars = dollars + (cents/100);
+                this.cents = cents % 100;
+            }
+        }
+        else {
+            System.out.println("! ERROR: Dollars and cents must be positive. " +
+                               "Setting dollars and cents to default values of 0.");
+            this.dollars = 0;
+            this.cents = 0;
+        }
+    }
 
     // Getters & Setters
 
     /**
-     * Getters for private variables
+     * Getter for dollars value
      */
+    public long getDollars() {
+        return dollars;
+    }
+
+    /**
+     * Getter for cents value
+     */
+    public long getCents() {
+        return cents;
+    }
 
     // Public Methods
 
     /**
      * toString() method returns a formatted string for printing
      */
+    public String toString() {
+        String displayCents;
+        // Add leading zeros to cents if necessary.
+        if (cents < 10) {
+            displayCents = "0" + cents;
+        }
+        else {
+            displayCents = "" + cents;
+        }
+        return ("$" + dollars + "." + displayCents);
+    }
 
     /**
      * add(Money value) binary operator
@@ -33,6 +83,36 @@ public class Money {
         Money result = new Money();
         result.cents = cents + value.cents;
         result.dollars = dollars + value.dollars;
+        // Only need to check if cents is greater than 100 since the
+        // largest value that can result from an addition is 198
+        if (result.cents >= 100) {
+            ++result.dollars;
+            result.cents -= 100;
+        }
         return result;
     }
+
+    /**
+     * subtract(Money value) binary operator
+     * returns the difference between object and value
+     * returns an error if value is larger than the object calling the method
+     */
+    public Money subtract(Money value)
+    {
+        Money result = new Money();
+        // Find the total cents values for the subtrahend (value) and the minuend (this), and compare them to make sure
+        // that the subtrahend is smaller than the minuend.
+        long myCentsAmount = (this.dollars * 100) + this.cents;
+        long valueCentsAmount = (value.dollars * 100) + value.cents;
+        long differenceBetween = myCentsAmount - valueCentsAmount;
+        if (differenceBetween < 0) { // Subtraction will result in a negative money value
+            System.out.println("! ERROR: Subtraction can't result in a negative Money value! " +
+                    "Setting dollars and cents to default values of 0.");
+        }
+        else { // Proper values for subtraction
+            result.dollars = differenceBetween / 100;
+            result.cents = differenceBetween % 100;
+        }
+        return result;
+        }
 }
